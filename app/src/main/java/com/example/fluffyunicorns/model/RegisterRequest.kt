d68755d011a88fca7283888d004dc4e56f4bea88
Ngoc Thach <ngocthachnguyen4399@gmail.com>
2024-12-16 19:23:06 +0700

    change font appropriately

--- app/src/main/java/com/example/fluffyunicorns/model/RegisterRequest.kt
+++ app/src/main/java/com/example/fluffyunicorns/model/RegisterRequest.kt
@@ -1,12 +1,12 @@
 package com.example.fluffyunicorns.model
 
-data class RegisterRequest(
-    val firstName: String,
-    val lastName: String,
-    val email: String,
-    val phone: String,
-    val password: String
-)
+//data class RegisterRequest(
+//    val firstName: String,
+//    val lastName: String,
+//    val email: String,
+//    val phone: String,
+//    val password: String
+//)
 
 data class ApiResponse(
     val success: Boolean,
