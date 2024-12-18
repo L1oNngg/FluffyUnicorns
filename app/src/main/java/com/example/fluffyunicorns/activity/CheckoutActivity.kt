1a05dbccaee3e0dc7271a3109b76d06dca9777ae
Ngoc Thach <ngocthachnguyen4399@gmail.com>
2024-12-19 00:01:46 +0700

    add more intents to main flow

--- app/src/main/java/com/example/fluffyunicorns/activity/CheckoutActivity.kt
+++ app/src/main/java/com/example/fluffyunicorns/activity/CheckoutActivity.kt
@@ -14,13 +14,7 @@ import com.example.fluffyunicorns.R
 class CheckoutActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.ui_checkout)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
 
         val backIcon: ImageButton = findViewById(R.id.backIcon)
 
