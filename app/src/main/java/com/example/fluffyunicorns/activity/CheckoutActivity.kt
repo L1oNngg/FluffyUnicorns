a21a1c7454ddd4ffedd360157637f050ad565880
Ngoc Thach <ngocthachnguyen4399@gmail.com>
2024-12-18 23:26:57 +0700

    add ScrollView in ui_register.xml and ui_checkout.xml

--- app/src/main/java/com/example/fluffyunicorns/activity/CheckoutActivity.kt
+++ app/src/main/java/com/example/fluffyunicorns/activity/CheckoutActivity.kt
@@ -1,6 +1,9 @@
 package com.example.fluffyunicorns.activity
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,6 +19,13 @@ class CheckoutActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val backIcon: ImageButton = findViewById(R.id.backIcon)
+
+        backIcon.setOnClickListener {
+            val intent = Intent(this, ServiceActivity::class.java)
+            startActivity(intent)
         }
     }
 }
