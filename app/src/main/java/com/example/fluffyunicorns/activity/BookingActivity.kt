7e6ea10dea5c5ee5de836310b81e045e490f7951
SayHoang <sayhoang.work@gmail.com>
2024-12-19 10:32:09 +0700

    open Image in Booking

--- app/src/main/java/com/example/fluffyunicorns/activity/BookingActivity.kt
+++ app/src/main/java/com/example/fluffyunicorns/activity/BookingActivity.kt
@@ -1,6 +1,8 @@
 package com.example.fluffyunicorns.activity
 
+import android.app.Dialog
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,10 +14,29 @@ class BookingActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.ui_booking)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val imageView17 = findViewById<ImageView>(R.id.imageView17)
+        val imageView18 = findViewById<ImageView>(R.id.imageView18)
+        val imageView19 = findViewById<ImageView>(R.id.imageView19)
+        val imageView20 = findViewById<ImageView>(R.id.imageView20)
+
+        // Thêm sự kiện click cho từng ImageView
+        setOnClickListenerForImage(imageView17, R.drawable.room_05)
+        setOnClickListenerForImage(imageView18, R.drawable.room_06)
+        setOnClickListenerForImage(imageView19, R.drawable.room_07)
+        setOnClickListenerForImage(imageView20, R.drawable.room_08)
+    }
+
+    private fun setOnClickListenerForImage(imageView: ImageView, imageResource: Int) {
+        imageView.setOnClickListener {
+            val dialog = Dialog(this)
+            dialog.setContentView(R.layout.dialog_image_fullscreen)
+
+            // Gán ảnh vào ImageView trong Dialog
+            val dialogImageView = dialog.findViewById<ImageView>(R.id.dialogImageView)
+            dialogImageView.setImageResource(imageResource)
+
+            dialog.show()
         }
     }
 }
